fd181b90063304f5d3ae6d5548030d9fa6e6996d
Mufeed V P <mufeedvp1@gmail.com>
2020-06-26 00:23:06 +0530

    connected mdb

--- mongodb_demo.js
+++ mongodb_demo.js
@@ -1,1 +1,11 @@
-con
+
+
+const MongoClient = require("mongodb");
+const connection_string = "mongodb://localhost:27017/"
+
+MongoClient.connect(connection_string,{useUnifiedTopology:true}, (err,client)=>{
+    if(err) throw error;
+    console.log("connected to mongodb")
+    client.close();
+});
+
