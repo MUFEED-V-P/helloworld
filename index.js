85a3f4e6e6f6605bb8b33df6abe03c26cd205baa
Mufeed V P <mufeedvp1@gmail.com>
2020-07-03 21:55:41 +0530

    crud operations

--- index.js
+++ index.js
@@ -1,7 +1,91 @@
+const MongoClient = require("mongodb").MongoClient;
+const connection_string = "mongodb://localhost:27017/"
+const cors = require("cors");
+
+
+
 const express = require('express');
-
-
 const app = express();
+
+//body parse
+var bodyParser = require("body-parser")
+//json parser
+var jsonParser = bodyParser.json();
+//url parser
+var urlencodeParser = bodyParser.urlencoded({extended:false});
+
+MongoClient.connect(connection_string,{useUnifiedTopology:true}, (err,client)=>{
+    if(err) throw error;
+    console.log("connected to mongodb")
+
+    //call the database
+    var db = client.db("create");
+
+    //list of documents 
+    app.get('/create', function(req,res){
+        db.collection("indro").find({}).toArray(function(err,data){
+            if (err)throw error
+            res.send(data)
+        })
+    })
+
+    //get single data
+    app.get('/create/:name', function(req,res){
+        let name = req.params.name;
+        db.collection("indro").find({name:name},{projection:{_id:0} }).toArray(function(err,data){
+            if (err)throw error
+            res.send(data)
+        })
+    })
+
+    //insert data
+    app.post('/create/:insert',jsonParser, function(req,res){
+        var qr = { 
+            name : req.body.name,
+            phone : req.body.phone,
+            password : req.body.pswd 
+        };
+
+        //let qr = ' insert into indro(name,phone,pswd) values( $name,$phone,$password ) ';
+        db.collection("indro").insertOne(qr,function(err,res){
+            if(err) throw error;
+            console.log("ok")
+        })
+        
+
+    })
+
+    //update data
+    app.patch('/create/:name',jsonParser, function(req,res){
+        let name = req.params.name;
+        var qr = { 
+            name : req.body.name,
+            phone : req.body.phone,
+            password : req.body.pswd 
+        };
+
+        //let qr = ' insert into indro(name,phone,pswd) values( $name,$phone,$password ) ';
+        db.collection("indro").updateMany({name:name},qr,function(err,res){
+            if(err) throw error;
+            console.log("updated")
+        })
+
+    })
+
+    //delet data
+    app.delete('/create/:name', function(req,res){
+        let name = req.params.name;
+        db.collection("indro").deleteMany({name:name},function(err,res){
+            if(err) throw error;
+            console.log("deleted");
+        })
+    })
+
+
+    
+   
+});
+
 
 app.get('/', function(req, res){
     
--- mongodb_demo.js
+++ mongodb_demo.js
@@ -1,11 +1,18 @@
 
 
-const MongoClient = require("mongodb");
+const MongoClient = require("mongodb").MongoClient;
 const connection_string = "mongodb://localhost:27017/"
 
+ 
 MongoClient.connect(connection_string,{useUnifiedTopology:true}, (err,client)=>{
     if(err) throw error;
     console.log("connected to mongodb")
+    
+
+
+
     client.close();
 });
+
+
 
